ac3b25b6c081208c965204848c9b88793bc69d21
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-11-06 19:41:11 +0600

    get method done

--- index.js
+++ index.js
@@ -24,7 +24,20 @@ async function run() {
 
         // post appointments
         app.post('/appointments', async (req, res) => {
-
+            const appointment = req.body;
+            const result = await appointmentCollection.insertOne(appointment);
+            res.json(result);
+        })
+        // get all appointments
+        app.get('/appointments', async (req, res) => {
+            const email = req.query.email;
+            const date = new Date(req.query.date).toLocaleDateString();
+            console.log(date);
+            const query = { email: email, date: date }
+            const cursor = appointmentCollection.find(query)
+            const result = await cursor.toArray();
+            console.log(result);
+            res.json(result);
         })
     }
     finally {
